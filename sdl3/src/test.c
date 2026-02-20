648ea2eda851b9237f10d8e10be23ef42bafe676
griddydev <GriddyDev@proton.me>
2026-02-19 20:25:33 -0800

    sdl display text two methods Text Engine and Surface to Texture pipeline NOTE main is a mess

--- sdl3/src/test.c
+++ sdl3/src/test.c
@@ -1,20 +1,31 @@
+//C Headers
+#include <stdio.h>
+//SDL Headers
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3_ttf/SDL_ttf.h>
 
-int main(int argc, char *argv[]) {
-	(void)argc;
-	(void)argv;
+static void DrawTextInBox(TTF_Text *text, SDL_FRect box) {
+	int textW, textH;
+	TTF_GetTextSize(text, &textW, &textH);
+	float posX, posY;
+	posX = box.x + ((box.w - (float)textW) / 2);
+	posY = box.y + ((box.w - (float)textH) / 2);
+	TTF_DrawRendererText(text, posX, posY);
+}
+
+int main(void) {
 	//Init SDL Video
 	if (!SDL_Init(SDL_INIT_VIDEO)) {
 		SDL_Log("SDL_Init Error: %s", SDL_GetError());
 		return 1;
 	}
-	
+	//Init SDL TTF
+	if (!TTF_Init()) {
+		SDL_Log("SDL_Init Error: %s", SDL_GetError());
+		return 1;
+	}
 	//Create Window and Renderer
-	
-	//Declare window and renderer pointer vars
-	//NOTE: SDL defaults to linear
-//	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"); 
 	SDL_Window *window = NULL;
 	SDL_Renderer *renderer = NULL;
 	//Create the window with renderer
@@ -23,6 +34,8 @@ int main(int argc, char *argv[]) {
         SDL_Quit();
         return 1;
     }
+	//Set render speed to match VSync (not max out CPU speed match monitor so like 60/120/420 hz)
+	SDL_SetRenderVSync(renderer, 1);
 	//Set window resizable
 	if (!SDL_SetWindowResizable(window, true)) {
 		SDL_Log("Failed to set window resizable: %s", SDL_GetError());
@@ -34,10 +47,25 @@ int main(int argc, char *argv[]) {
         SDL_Quit();
         return 1;
     }
+	//Text variables:
+	//Create Text Engine (SDL3 supercedes surface -> texture pipeline)
+	TTF_TextEngine *engine = TTF_CreateRendererTextEngine(renderer);
+	//load font
+	TTF_Font *font = TTF_OpenFont("fonts/Press_Start_2P/PressStart2P-Regular.ttf", 16);
+	//test text 
+	TTF_Text *helloText = TTF_CreateText(engine, font, "Hello World", 0);
+	//text color
+	TTF_SetTextColor(helloText, 45, 45, 90, 255);
+
+	//Big Bang stuff
+	SDL_Color bangColor = {25,125,225, 255};
+	TTF_Font *bangFont = TTF_OpenFont("fonts/Press_Start_2P/PressStart2P-Regular.ttf", 128);
+	SDL_Surface *surface = TTF_RenderText_Solid(bangFont, "TEST!", 0, bangColor);
+	SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+	SDL_DestroySurface(surface);
 	//The Loop
 	bool quit = false;
 	SDL_Event event;
-	
 	while (!quit) {
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_EVENT_QUIT) {
@@ -55,14 +83,27 @@ int main(int argc, char *argv[]) {
         SDL_FRect rect = { 220, 140, 200, 200 }; // x, y, width, height
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
         SDL_RenderFillRect(renderer, &rect);
+		if (!TTF_DrawRendererText(helloText, 100.0f, 100.0f)) {
+			SDL_Log("Shadow realm! Drawing failed: %s", SDL_GetError());
+		}
+		DrawTextInBox(helloText, rect);
+		//BigBang draw
+		SDL_FRect bigBangRect = { 960, 540, 192, 108};
+		SDL_RenderTexture(renderer, texture, NULL, &bigBangRect);
         // Present the backbuffer (Show what we drew)
         SDL_RenderPresent(renderer);
     }
-
 	//Cleanup
+	TTF_DestroyRendererTextEngine(engine);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+	//Text cleanup
+	TTF_DestroyText(helloText);
+	TTF_CloseFont(font);
+	TTF_Quit();
+	//Texture Cleanup
+	SDL_DestroyTexture(texture);
+	//quit
     SDL_Quit();
-
     return 0;
 }
