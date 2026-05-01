b3b5027e3c1667cc3d48839e40b926a810bbc22d
griddydev <GriddyDev@proton.me>
2026-04-30 20:03:07 -0700

    name defense play, create enums for plays

--- include/play_calling.h
+++ include/play_calling.h
@@ -12,6 +12,32 @@ void PlayCalling_Update(GameData *data);
 void PlayCalling_Render(const GameEngine *eng, const GameData *data);
 
 typedef enum {
+	OFFENSE_NONE,
+
+	OFFENSE_RUN,
+	OFFENSE_SHORT_PASS,
+	OFFENSE_LONG_PASS,
+	OFFENSE_KNEEL,
+	OFFENSE_KICK,
+	OFFENSE_PUNT,
+
+	OFFENSE_COUNT
+} OffensePlays;
+
+typedef enum {
+	DEFENSE_NONE,
+
+	DEFENSE_BASE,
+	DEFENSE_MAN,
+	DEFENSE_COVER,
+	DEFENSE_PREVENT,
+	DEFENSE_GOAL_LINE,
+	DEFENSE_BLITZ,
+
+	DEFENSE_COUNT
+} DefensePlays;
+
+typedef enum {
 	PLAY_CALLING_UI_NONE,
 
 	PLAY_CALLING_UI_BUTTON1,
@@ -26,6 +52,7 @@ typedef enum {
 
 	PLAY_CALLING_UI_COUNT
 } PlayCallingUIElement;
+
 
 typedef struct {
 	ScoreboardData scoreboard;
--- src/play_calling.c
+++ src/play_calling.c
@@ -1,3 +1,22 @@
+
+//Six options on offense and defense
+//Off: Run, Short Pass, Long Pass, Kneel, Kick, Punt
+//Def: Base, Man, Cover, Prevent, Goaline, Blitz
+
+//Run: Low fumble %, likely to gain low yards
+//Short Pass: Standard fumble %, less likely than run but better yardage
+//Long Pass: high int %, low likelyhood of success but results in a large play
+//Kneel: Lose a yard and increase a down - only fails on two successive 0 rolls (1 in 1000)
+//Kick: Attempt a field goal. % changes based on distance to goal
+//Punt: Punt the ball - 1 % chance he fumbles the punt - another 3% chance the recovering team fumbles - calc result seperate
+
+//Base:    0RN 0SP 0LP 
+//Man:     +RN +SP -LP
+//Cover:   -RN +SP +LP
+//Prevent: -RN 0SP +LP
+//Goaline  +RN -SP -LP
+//Blitz    ?RN -SP ?LP
+
 #include "play_calling.h"
 
 #include <stdlib.h>
